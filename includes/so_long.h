f695d3e5deea316f5124033907145112a5ab3b5d
aautin <ezagof@gmail.com>
2024-01-23 21:58:02 +0100

    Commit to squash
    - create a maps folder with maps for every errors' cases
    - handle these errors cases during parsing (no leaks)
    	-> not .ber extension	-> map isnt a rectangle
    	-> file is empty	-> problem of allocations
    	-> an invalid character	-> no right to read the file
    	-> argc != 2
    - now, in no-error of above cases, we obtain a **char of the map

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,15 +6,31 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 17:42:16 by aautin            #+#    #+#             */
-/*   Updated: 2024/01/22 18:58:02 by aautin           ###   ########.fr       */
+/*   Updated: 2024/01/23 21:23:48 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include "../libft/includes/libft_extended.h"
-#include "../mlx/mlx.h"
-#include "../mlx/mlx_int.h"
+# include "../libft/includes/libft_extended.h"
+# include "../mlx/mlx.h"
+# include "../mlx/mlx_int.h"
+
+# define TRUE 1
+# define FALSE 0
+
+typedef struct s_coords
+{
+	int	x;
+	int	y;
+}	t_coords;
+
+typedef struct s_game
+{
+	char	**map;
+}	t_game;
+
+char	**file_to_map(char *mapfile);
 
 #endif
